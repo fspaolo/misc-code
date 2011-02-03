8d3f8eaab525fb381c83c21348c7b91b8bc160f6
Fernando Paolo <fspaolo@gmail.com>
2011-02-02 22:33:57 -0800

    updated header

--- idr/readidr_ra1.f95
+++ idr/readidr_ra1.f95
@@ -16,7 +16,7 @@
 ! ------
 ! Undefined surf values are set to -9999
 ! Undefined corrections are set to 32767
-! For GFO use inc: 1 (more points) or 2 (better inc)
+! For GFO use inc: 2 (better inc) or 1 (more points)
 ! For Geosat/GM: use inc 2
 ! For Seasat, Geosat/ERM, ERS-1/2: use inc 3
 !
