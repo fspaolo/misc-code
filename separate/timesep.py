e335d4ec5c8574ad7c86d9b13bc4cac46bc9c78b
Fernando Paolo <fspaolo@gmail.com>
2011-07-13 18:22:07 -0700

    added separate by seasons capability

--- separate/timesep.py
+++ separate/timesep.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 doc = """\
- Read utc85 (from HDF5/ASCII) and separate by: year, month or month-window.
+ Given utc85 time separate files by: years, seasons, months or month-window.
 """ 
 """
  Fernando Paolo <fpaolo@ucsd.edu>
@@ -24,9 +24,11 @@ parser.add_argument('file', nargs='+', help='HDF5/ASCII file[s] to read')
 parser.add_argument('-c', dest='utc85col', type=int, default=1, 
     help='column of utc85 variable (0,1..) [default: 1]')
 group.add_argument('-m', dest='month', action='store_const', const=True,
-    help='separate by month [default: by year]')
-group.add_argument('-w', dest='m', nargs=2, type=int, metavar=('MON1', 'MON2'),
-    help='separate by month-window (MON=1,2..12)')
+    help='separate by months [default: by years]')
+group.add_argument('-w', dest='window', nargs=2, type=int, metavar=('MON1', 'MON2'),
+    help='separate by month-window (MON=1,2..12, increasing order)')
+group.add_argument('-s', dest='season', action='store_const', const=True,
+    help='separate by seasons [default: by years]')
 parser.add_argument('-d', dest='dir', action='store_const', const=True, \
     default=False, help='create directories for output files [default: no]')
 parser.add_argument('-a', dest='ascii', default=False, action='store_const',
@@ -36,7 +38,8 @@ args =  parser.parse_args()
 files = args.file
 col = args.utc85col
 month = args.month
-window = args.m
+window = args.window
+season = args.season
 dir = args.dir
 ascii = args.ascii
 
@@ -51,14 +54,16 @@ else:
     print 'reading and writing HDF5'
 
 print 'files to read:', len(files)
-print 'column of utc85:', col
+print 'time (utc85) column:', col
 if month is not None:
-    print 'separate by: month'
+    print 'separate by: months'
 elif window is not None:
     M1, M2 = window
     print 'separate by: month-window', mname[M1], mname[M2]
+elif season is not None:
+    print 'separate by: seasons'
 else:
-    print 'separate by: year'
+    print 'separate by: years'
 print 'create directories:', dir
 
 
@@ -86,6 +91,27 @@ class Utc85:
         
     def getday(self):
         return np.array([d.day for d in self.date])
+
+
+def what_season(month):
+    """Given a month finds the 3-month block of the season.
+    """
+    season1 = [12, 1, 2]     # Dec--Feb -> summer SH
+    season2 = [3, 4, 5]      # Mar--May -> Autumn SH 
+    season3 = [6, 7, 8]      # Jun--Aug -> winter SH
+    season4 = [9, 10, 11]    # Sep--Nov -> Spring SH
+
+    if month in season1:
+        return season1
+    elif month in season2:
+        return season2
+    elif month in season3:
+        return season3
+    elif month in season4:
+        return season4
+    else:
+        print 'not a valid month from 1 to 12!'
+        return None, None, None    # none existent range
 
 
 def print_date(utc85, N):
@@ -120,31 +146,48 @@ def main():
         #print_date(utc85, 10)         # to test the output
         #continue
 
-        if month is not None or window is not None:
-            epochs = utc85.getmonth()  # utc85 -> months
+        ### convert utc85 column to epochs
+        if month is not None or window is not None or season is not None:
+            epochs = utc85.getmonth()           # utc85 -> months
         else:
-            epochs = utc85.getyear()   # utc85 -> years
+            epochs = utc85.getyear()            # utc85 -> years
 
-        if not np.add.reduce(epochs) > 0: continue  # go to next file (!!!)
+        if not np.add.reduce(epochs) > 0:       # if no epochs 
+            continue                            # go to next file (!!!)
 
         while True:
 
+            ### find specific epoch
+
             if window is not None:
+                # the month-window
                 IND, = np.where((M1 <= epochs) & (epochs <= M2))
-            else:
+            elif season is not None:
+                # first season occurrence
                 nonzeros = epochs[epochs!=0]
                 EPOCH = nonzeros[0]             # take the first non 0 epoch
-                IND, = np.where(epochs == EPOCH)
+                M1, M2, M3 = what_season(EPOCH) # 3-month season
+                IND, = np.where((epochs == M1) | (epochs == M2) | (epochs == M3))
                 epochs[IND] = 0                 # mark wiht zero all taken epochs
+            else:
+                # first month or year occurrence
+                nonzeros = epochs[epochs!=0]
+                EPOCH = nonzeros[0]             
+                IND, = np.where(epochs == EPOCH)
+                epochs[IND] = 0                
 
             path, file = os.path.split(f)
             fname, ext = os.path.splitext(file)
+
+            ### output directory or file
 
             if dir is True:                     # create dir
                 if month is not None:
                     dirname = mname[EPOCH]
                 elif window is not None:
                     dirname = mname[M1] + mname[M2]
+                elif season is not None:
+                    dirname = mname[M1] + mname[M3]
                 else:
                     dirname = str(EPOCH)
                 outdir = os.path.join(path, dirname) 
@@ -158,9 +201,13 @@ def main():
                     fname += ('_' + mname[EPOCH] + ext)
                 elif window is not None:
                     fname += ('_' + mname[M1] + mname[M2] + ext)
+                elif season is not None:
+                    fname += ('_' + mname[M1] + mname[M3] + ext)
                 else:
                     fname += ('_' + str(EPOCH) + ext)
                 outfile = os.path.join(path, fname)
+
+            ### save data
 
             if IND.shape[0] > 0:
                 if ascii:
