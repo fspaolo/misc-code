34204a9663276e1da626683035d5774368e424ea
Fernando Paolo <fspaolo@gmail.com>
2011-09-06 09:53:49 -0700

    added specific funcs/options for `Array`, `CArray` and `Table`

--- convert/txt2h5.py
+++ convert/txt2h5.py
@@ -14,16 +14,16 @@ Examples
 --------
 To see the available options::
 
-$ python txt2h5.py -h
+  $ python txt2h5.py -h
 
 To convert 3 `cols` of several ASCII files given the name of each 
 `field` (column) and specifying a compression lib::
 
-$ python txt2h5.py -c 1,2,3 -f lon,lat,elev -l zlib /path/to/files/*.txt
+  $ python txt2h5.py -c 1,2,3 -f lon,lat,elev -l zlib /path/to/files/*.txt
 
 To convert some `fields` of an HDF5 `Table` file to ASCII::
 
-$ python txt2h5.py -f time,lon,lat,temp /path/to/files/*.h5
+  $ python txt2h5.py -f time,lon,lat,temp /path/to/files/*.h5
 
 """
 # Fernando Paolo <fpaolo@ucsd.edu>
@@ -40,24 +40,33 @@ import tables as tb
 parser = ap.ArgumentParser()
 parser.add_argument('files', nargs='+',
     help='file[s] to convert [ex: /path/to/files/*.ext]')
-parser.add_argument('-v', dest='verbose', default=False, action='store_const',
-    const=True, help='for verbose [default: run silent]')
+parser.add_argument('-v', dest='verbose', default=False, 
+    action='store_const', const=True, 
+    help='for verbose [default: run silent]')
+parser.add_argument('-arr', dest='array', default=False, 
+    action='store_const', const=True, 
+    help='creates an HDF5 Array [default: Table]')
+parser.add_argument('-carr', dest='carray', default=False, 
+    action='store_const', const=True, 
+    help='creates an HDF5 CArray w/compression [default: Table]')
 parser.add_argument('-f', dest='fields', default=[],
     help='names to be used for the `fields` (columns), ex: -f x,y,z '
-    '[default: data]')
+    '[default: no columns]')
 parser.add_argument('-d', dest='formats', default=[],
-     help='data format of the resulting array, ex: -d i4,f8,f4 '
-     '[default: f8]')
+    help='data format of the resulting array, ex: -d i4,f8,f4 '
+    '[default: f8]')
 parser.add_argument('-c', dest='usecols', default=(),
     help='ASCII columns to convert, ex: -c 3,0,1 '
     '[default: all cols]')
 parser.add_argument('-l', dest='complib', default='zlib',
-    help='compression library to be used: zlib, lzo, bzip2, blosc' 
+    help='compression library to be used: zlib, lzo, bzip2, blosc ' 
     '[default: zlib]')
 
 args = parser.parse_args()
 files = args.files
 verbose = args.verbose
+array = args.array
+carray = args.carray
 fields = args.fields
 formats = args.formats
 usecols = args.usecols
@@ -75,55 +84,129 @@ if usecols:
 dtype = {'names': fields, 'formats': formats}
 
 
-def h5_to_txt(fname, fields=[]):
-    """Converts HDF5 file with Table/Array to ASCII file.
+def txt_to_h5_tab(fname, dtype={}, usecols=(), complib='zlib'):
+    """Converts ASCII column data file to HDF5 Table.
+
+    It uses `dtype`, a dictionary defining the `names` and `formats`
+    of the ASCII columns, to create a `Table` with the data in it.
+    """
+    if os.path.getsize(fname) == 0:
+        print 'file is empty!'
+        return
+    if not dtype['formats']:
+        raise IOError, 'must specify `fields`'
+    data = np.loadtxt(fname, usecols=usecols)  # data in-memory
+    h5f = tb.openFile(os.path.splitext(fname)[0] + '.h5', 'w')
+    print 'Table (%d, %d)' % data.shape
+    print 'fields:', dtype['names'], dtype['formats']
+    print 'compression lib:', complib
+    data = np.rec.fromrecords(data, dtype=dtype)  # arr to rec
+    filters = tb.Filters(complib=complib, complevel=9)
+    outdata = h5f.createTable(h5f.root, 'data', data, filters=filters)
+    h5f.close()
+
+
+def h5_tab_to_txt(fname, fields=[]):
+    """Converts HDF5 Table to ASCII file.
     
-    If the HDF5 is a `Table` and `fields` are passed, only those 
-    columns will be converted. If HDF5 is an `Array`, all columns
-    are converted.
+    If `fields` are passed, only those columns are converted, 
+    otherwise convert them all.
     """
     if os.path.getsize(fname) == 0:
         print 'file is empty!'
         return
     h5f = tb.openFile(fname)
     for leaf in h5f.walkNodes('/', classname='Leaf'):
-        if isinstance(leaf, tb.Table):
-            if not fields:
-                fields = leaf.colnames
-            print 'Table (%d, %d)' % (leaf.nrows, len(fields))
-            print 'fields:', fields
-            data = np.array([leaf.col(col) for col in fields])
-            data = data.T
-        else:
-            print 'Array (%d, %d)' % leaf.shape
-            data = leaf.read()
+        if not isinstance(leaf, tb.Table):
+            raise TypeError, 'object is not a Table!'
+        if not fields:
+            fields = leaf.colnames
+        print 'Table (%d, %d)' % (leaf.nrows, len(fields))
+        print 'fields:', fields
+        data = np.array([leaf.col(col) for col in fields])
+        data = data.T
     np.savetxt(os.path.splitext(fname)[0] + '.txt', data, fmt='%.6f')
     h5f.close()
 
 
-def txt_to_h5(fname, dtype=dtype, usecols=(), complib='zlib'):
-    """Converts ASCII file with data columns to HDF5 file.
+def txt_to_h5_arr(fname, dtype={}, usecols=()):
+    """Converts ASCII column data file to HDF5 Array.
 
     If `dtype` (a dictionary) with `fields` is passed the ASCII data 
-    is converted to a `Table`, otherwise is converted to a 2D `Array` 
-    mirroring the ASCII file.
+    columns are converted to 1D `Arrays`, otherwise they are converted 
+    to a 2D `Array` mirroring the ASCII file.
     """
     if os.path.getsize(fname) == 0:
         print 'file is empty!'
         return
-    data = np.loadtxt(fname, usecols=usecols)  # data in-memory
+    data = np.loadtxt(fname, usecols=usecols)    # data in-memory
     h5f = tb.openFile(os.path.splitext(fname)[0] + '.h5', 'w')
     if not dtype['names']:
-        print 'Array (%d, %d)' % data.shape
-        outdata = h5f.createArray(h5f.root, 'data', data)
+        print '2D Array (%d, %d)' % data.shape
+        outdata = h5f.createArray('/', 'data', data)
     else:
-        print 'Table (%d, %d)' % data.shape
+        print '1D Arrays (%d x %d)' % data.shape
+        print 'fields:', dtype['names'], dtype['formats']
+        group = h5f.createGroup('/', 'data')
+        for col, field in enumerate(dtype['names']):
+            outdata = h5f.createArray(group, field, data[:,col])
+    h5f.close()
+
+
+def txt_to_h5_carr(fname, dtype={}, usecols=(), complib='zlib'):
+    """Converts ASCII column data file to HDF5 CArray w/compression.
+
+    If `dtype` (a dictionary) with `fields` is passed the ASCII data 
+    columns are converted to 1D `Arrays`, otherwise they are converted 
+    to a 2D `Array` mirroring the ASCII file.
+    """
+    if os.path.getsize(fname) == 0:
+        print 'file is empty!'
+        return
+    data = np.loadtxt(fname, usecols=usecols)    # data in-memory
+    h5f = tb.openFile(os.path.splitext(fname)[0] + '.h5', 'w')
+    atom = tb.Float64Atom()
+    filters = tb.Filters(complib=complib, complevel=9)
+    if not dtype['names']:
+        print '2D CArray (%d, %d)' % data.shape
+        print 'compression lib:', complib
+        shape = data.shape
+        dataout = h5f.createCArray('/','data', atom=atom, shape=shape,
+                                   filters=filters)
+        dataout[:] = data
+    else:
+        print '1D CArrays (%d x %d)' % data.shape
         print 'fields:', dtype['names'], dtype['formats']
         print 'compression lib:', complib
-        data = np.rec.fromrecords(data, dtype=dtype)  # arr to rec
-        filters = tb.Filters(complib=complib, complevel=9)
-        outdata = h5f.createTable(h5f.root, 'data', data, filters=filters)
+        group = h5f.createGroup('/', 'data')
+        for col, field in enumerate(dtype['names']):
+            shape = data[:,col].shape
+            dataout = h5f.createCArray(group, field, atom=atom, shape=shape,
+                                       filters=filters)
+            dataout[:] = data[:,col]
     h5f.close()
+
+
+### TODO
+def h5_arr_to_txt(fname, fields=[]):
+    """Converts HDF5 Array to ASCII file.
+    
+    If `fields` are passed, only those columns are converted, 
+    otherwise convert them all.
+    """
+    pass
+    '''
+    if os.path.getsize(fname) == 0:
+        print 'file is empty!'
+        return
+    h5f = tb.openFile(fname)
+    data = [leaf.read() for leaf in h5f.walkNodes('/') \
+            if isinstance(leaf, tb.Array)]
+    #print 'Table (%d, %d)' % (leaf.nrows, len(fields))
+    #print 'fields:', fields
+    np.savetxt(os.path.splitext(fname)[0] + '.txt', data, fmt='%.6f')
+    h5f.close()
+    '''
 
 
 def main():
@@ -134,10 +217,18 @@ def main():
             print 'file:', f
         if mime == 'text/plain':
             print 'ASCII -> HDF5 ...'
-            txt_to_h5(f, dtype, usecols, complib)
+            if array:
+                txt_to_h5_arr(f, dtype, usecols)
+            elif carray:
+                txt_to_h5_carr(f, dtype, usecols, complib)
+            else:
+                txt_to_h5_tab(f, dtype, usecols, complib)
         else:
             print 'HDF5 -> ASCII ...'
-            h5_to_txt(f, dtype['names'])
+            if array:
+                h5_arr_to_txt(f, dtype['names'])
+            else:
+                h5_tab_to_txt(f, dtype['names'])
     print 'done.'
 
 if __name__ == '__main__':
