d113f64a2193f0878370267bec801265b96e4c6b
Fernando Paolo <fspaolo@gmail.com>
2011-11-05 19:10:15 -0700

    added function for formatted out file name

--- x2sys/x2sys.py
+++ x2sys/x2sys.py
@@ -2,7 +2,7 @@
 Script to automatize the input/output of GMT's x2sys program.
 
 Read several HDF5/ASCII files and cross them with a given *reference* 
-file. For example, for a `reffile_0.ext` and `infile_1.ext, infile_2.ext, 
+file. Example, for a `reffile_0.ext` and `infile_1.ext, infile_2.ext, 
 infile_3.ext, ...`, we would have:
 
     reffile0.ext - infile1.ext, 
@@ -19,6 +19,7 @@ from a configuration file `x2sys.ini` that can be edited.
 
 import os
 import sys
+import re
 import argparse as ap
 import configobj as co
 import numpy as np
@@ -42,9 +43,9 @@ values = '-' + config['cross']['values']
 
 # parse command line arguments
 parser = ap.ArgumentParser()
-parser.add_argument('file', nargs='*', help='HDF5/ASCII file[s] to read')
+parser.add_argument('files', nargs='*', help='HDF5/ASCII file[s] to read')
 parser.add_argument('-r', dest='reffile', default=None, 
-    help='reference file to cross w/others: reffile - infiles')  
+    help='reference file to cross w/others: file_ref - files_in')  
 parser.add_argument('-k', dest='skiprows', type=int, default=4,
     help='skip the first SKIPROWS lines from output file [default: 4]')
 parser.add_argument('-o', dest='cols', nargs='*', default= \
@@ -59,13 +60,11 @@ parser.add_argument('-i', dest='init', default=False, action='store_const',
 args = parser.parse_args()
 
 if not args.init and args.reffile is None:
-    print "x2sys.py: error: argument -r is required"
-    sys.exit()
-if not args.init and not args.file:
-    print "x2sys.py: error: no input files"
-    sys.exit()
+    raise IOError("x2sys.py: argument `-r` is required")
+if not args.init and not args.files:
+    raise IOError("x2sys.py: no input files")
 
-files = args.file
+files_in = args.files
 file_ref = args.reffile
 skiprows = args.skiprows
 usecols = args.cols
@@ -74,13 +73,9 @@ ascii = args.ascii
 
 # remove reffile from the input file list
 # to avoid crossing reffile w/itself
-if file_ref in files: 
-    #files.remove(file_ref) 
-    print 'crossing reference file with it self!!!'
-    if not files:
-        print 'ref file:', file_ref
-        print 'not crossing ref file w/itself!'
-        sys.exit()
+if file_ref in files_in: 
+    #files_in.remove(file_ref) 
+    print 'attempting to cross reference file with it self!!!'
 
 
 def readh5(fname, inmemory=False):
@@ -104,7 +99,7 @@ def saveh5(fname, data):
     fout = tb.openFile(fname, 'w')
     shape = data.shape
     atom = tb.Atom.from_dtype(data.dtype)
-    filters = tb.Filters(complib='blosc', complevel=5)
+    filters = tb.Filters(complib='blosc', complevel=9)
     dout = fout.createCArray(fout.root,'data', atom=atom, shape=shape,
                              filters=filters)
     dout[:] = data[:] 
@@ -124,53 +119,68 @@ def hdf2txt_tmp(fname):
     return f
 
 
-def ofname_without_ext(fname1, fname2):
-    """The name of output crossover file.
+def fname_out_no_ext(fname1, fname2):
+    """The name of output crossover file: fname1_fname2.
     """
-    pathfname1, ext1 = os.path.splitext(fname1)
-    pathfname2, ext2 = os.path.splitext(fname2)
-    path1, fname1 = os.path.split(pathfname1)
-    path2, fname2 = os.path.split(pathfname2)
-    return os.path.join(path2, fname1 + '-' + fname2)
+    fname1, _ = os.path.splitext(os.path.basename(fname1))
+    path2, fname2 = os.path.split(os.path.splitext(fname2)[0])
+    return os.path.join(path2, fname1 + '_' + fname2)
+
+
+def fname_out_no_ext2(fname1, fname2):
+    """The name of output crossover file: formatted.
+    """
+    path2 = os.path.split(fname2)[0]
+    fname1, _ = os.path.splitext(os.path.basename(fname1))
+    sat, t1, r1 = fname1.split('_')[:3]
+    t2 = fname2.split('_')[1]
+    return os.path.join(path2, '_'.join([sat, t1, t2, r1]))
 
 #------------------------------------------------------------------------
 
-# generate data base with given settings (x2sys.ini) 
-if args.init:
-    #os.system('sudo rm -rfv /opt/local/share/gmt/x2sys/ALTIM')
-    os.system('sudo x2sys_init %s %s %s %s %s %s %s %s -V' % \
-              (tagname, deffile, extension, overwrite, distcalc, \
-               coordinates, distunits, maxgap))
-    sys.exit()
+def main():
+    # generate data base with given settings (x2sys.ini) 
+    if args.init:
+        #os.system('sudo rm -rfv /opt/local/share/gmt/x2sys/ALTIM')
+        os.system('sudo x2sys_init %s %s %s %s %s %s %s %s -V' % \
+                  (tagname, deffile, extension, overwrite, distcalc, \
+                   coordinates, distunits, maxgap))
+        sys.exit()
+        
+    print 'files to cross:', len(files_in) + 1
 
-    
-print 'files to cross:', len(files) + 1
+    nfiles = 0
+    nxovers = 0
+    for file_i in files_in:
 
-nfiles = 0
-nxovers = 0
-for file_i in files:
+        #file_out = fname_out_no_ext(file_ref, file_i)
+        file_out = fname_out_no_ext2(file_ref, file_i)
 
-    file_out = ofname_without_ext(file_ref, file_i)
+        print 'x2sys_cross: processing', file_ref, '-', file_i, '...'
+        os.system('/opt/local/bin/x2sys_cross %s %s -T%s %s %s %s > %s' % \
+            (file_ref, file_i, tagname, coe, interpolation, values, file_out))
 
-    print 'x2sys_cross: processing', file_ref, '-', file_i, '...'
-    os.system('/opt/local/bin/x2sys_cross %s %s -T%s %s %s %s > %s' % \
-              (file_ref, file_i, tagname, coe, interpolation, values, file_out))
+        #print 'file header:'
+        #os.system('head -n4 %s' % file_out)
+        try:
+            data = np.loadtxt(file_out, skiprows=skiprows, usecols=usecols)
+            #np.savetxt(file_out + '.txt', data, fmt='%.6f')
+            saveh5(file_out + '.h5', data)
+            os.remove(file_out)
+            nxovers += data.shape[0]
+            nfiles += 1
+            print 'number of crossovers:', data.shape[0]
+        except:
+            os.remove(file_out)
+            file_out = None
+            print 'no crossovers found!'
+            #raise  # shows the error
+        
 
-    #print 'file header:'
-    #os.system('head -n4 %s' % file_out)
-    try:
-        data = np.loadtxt(file_out, skiprows=skiprows, usecols=usecols)
-        np.savetxt(file_out + '.txt', data, fmt='%.6f')
-        saveh5(file_out + '.h5', data)
-        nxovers += data.shape[0]
-        nfiles += 1
-        print 'number of crossovers:', data.shape[0]
-    except:
-        print 'no crossovers found!'
-        #raise  # shows the error
-    
+    print 'done.'
+    print 'total crossovers:', nxovers
+    print 'crossover files created:', nfiles
+    print 'last output file (.h5):', file_out
 
-print 'done!'
-print 'total crossovers:', nxovers
-print 'crossover files created:', nfiles
-print 'last output file:', file_out + '.h5'
+if __name__ == '__main__':
+    main()
